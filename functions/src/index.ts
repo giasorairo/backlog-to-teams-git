3e89ff004a982baff7032a658cfcc0dc4c3b8e0a
takayanagi_marietta <takayanagi.t@marietta.co.jp>
2020-12-28 10:17:39 +0900

    [add]expressでapi作成

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,35 @@
 import * as functions from 'firebase-functions';
+import * as express from "express";    // こう
+
+const app = express();
+
+app.get('/hello', (req, res) => {
+  // レスポンスの設定
+  res.send('Hello express!')
+});
+
+// エンドポイントを追加
+app.get('user/:userId', (req, res) => {
+  const users = [
+    { id: 1, name: 'りゅうおう' },
+    { id: 2, name: 'ハーゴン' },
+    { id: 3, name: 'バラモス' },
+    { id: 4, name: 'ゾーマ' },
+    { id: 5, name: 'ピサロ' },
+  ];
+  // req.params.userIdでURLの後ろにつけた値をとれる
+  const targetUser = users.find((user) => user.id === Number(req.params.userId));
+  res.send(targetUser);
+});
+
+
+const api = functions.https.onRequest(app);
+module.exports = { api };
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
